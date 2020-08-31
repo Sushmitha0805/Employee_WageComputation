6412d0a4a11a38804ca135356f28138f0d9158e9
Sushmitha0805 <69563297+Sushmitha0805@users.noreply.github.com>
2020-08-31 12:19:12 +0530

    Update MultipleCompany.java

--- MultipleCompany.java
+++ MultipleCompany.java
@@ -1,80 +1,67 @@
-public class MultipleCompany{
+public class MultipleCompany1{
 
-  final int IS_PART_TIME=1;
-  final int IS_FULL_TIME=2;
-  final int IS_ABSENT=0;
-  
-  private final String CompanyName;
-  private final int EMP_WAGE_PER_HR;
-  private final int NUM_OF_WORKING_DAYS;
-  private int maxhrs;
- 
-  public MultipleCompany(final String CompanyName , final int EMP_WAGE_PER_HR, final int NUM_OF_WORKING_DAYS,final int maxhrs)
-{
-        this.CompanyName=CompanyName;
-        this.EMP_WAGE_PER_HR=EMP_WAGE_PER_HR;
-        this. NUM_OF_WORKING_DAYS= NUM_OF_WORKING_DAYS;
-        this.maxhrs=maxhrs;
-}
-  public String getCompanyName(){
-   return CompanyName; 
-   }
-  public int getEMP_WAGE_PER_HR(){
-    return EMP_WAGE_PER_HR;
-   }
-  public int getmaxhrs(){
-   return maxhrs;
-   }
-  public int getNUM_OF_WORKING_DAYS(){ 
-   return NUM_OF_WORKING_DAYS;
-   }
+  public static final int IS_PART_TIME=1;
+  public static final int IS_FULL_TIME=2;
+  public static final int IS_ABSENT=0;
 
-    public int EmpWageComputation(){
-         int empHrs=0;
-         int Salary=0;
-         int TotalWorkingDays=0;
-         int TotalEmpHr=0;
-        
-         //Computation
-           while(TotalEmpHr <= maxhrs && TotalWorkingDays < NUM_OF_WORKING_DAYS)
-          {
-              TotalWorkingDays++;
-           	int empCheck=(int)Math.floor(Math.random()*10)%3;
-			 
-         switch( empCheck ){
-             case IS_FULL_TIME:
-				      System.out.println("Employee Is full time");
-			         empHrs=8;
-                   break;
-     	      case IS_PART_TIME:
-				       System.out.println("Employee is Part time Employee");
-					     empHrs=4;
-                  break;
-            case  IS_ABSENT:
-			           System.out.println("Employee Is Absent");
-					     empHrs=0;
-                    break;
-             default:
-                    System.out.println("invalid");
-		}
-           TotalEmpHr=(TotalEmpHr+empHrs);
-                        System.out.println("Day:"+TotalWorkingDays+" Emp Hours:"+ empHrs);
+  private final String company;
+  private final int empRatePerHour;
+  private final int numOfWorkingDays;
+  private final int maxHoursPerMonth;
+  private int totalEmpWage;
 
-     }
-                Salary=(TotalEmpHr*EMP_WAGE_PER_HR);
-                 return Salary;                
+  public MultipleCompany1(String company, int empRatePerHour, int numOfWorkingDays,int maxHoursPerMonth){
+   this.company=company;
+   this.empRatePerHour=empRatePerHour;
+   this.numOfWorkingDays=numOfWorkingDays;
+   this.maxHoursPerMonth=maxHoursPerMonth;
  }
 
-  public static void main(String args[]) {
 
-              MultipleCompany AXISEmp= new MultipleCompany("AXIS", 25, 25, 100);
-              int AXISWages=AXISEmp.EmpWageComputation();
-                System.out.println("Axis Employee salary: "+AXISWages);
 
-              MultipleCompany SBIEmp= new MultipleCompany("SBI", 25, 25, 100);
-              int SBIWages=SBIEmp.EmpWageComputation();
-                System.out.println("SBI Employee salary: "+SBIWages);
-      }
+      private int computeEmpWage(){
+     //variables
+     int empHrs=0;
+     int totalEmpHrs=0;
+     int totalWorkingDays=0;
+     //computation
+     while(totalEmpHrs<=maxHoursPerMonth && totalWorkingDays  < numOfWorkingDays){
+        int empCheck=(int)Math.floor(Math.random()*10)%3;
+        switch(empCheck){
+           case IS_PART_TIME:
+              empHrs=4;
+              break;
+           case IS_FULL_TIME:
+              empHrs=8;
+              break;
+           case IS_ABSENT:
+              empHrs=0;
+              break;
+           default:
+                       System.out.println("invalid");
+                         System.out.println("invalid");
+        }
+        totalEmpHrs+=empHrs;
+        System.out.println("Day#: " +totalWorkingDays + "EmpHr:" +empHrs);
+     }
+      totalEmpWage=totalEmpHrs*empRatePerHour;
+     System.out.println("Total Emp Wage for comapny: " +company+ " is:" +totalEmpWage);
+      return totalEmpWage;
+ }
+  //override
+  public String toString(){
+     return "Total Emp Wage for Company : " +company+ "is:" +totalEmpWage;
+     }
 
+   public static void main(String[] args){
+        MultipleCompany1 DMART = new MultipleCompany1 ("DMART",20,20,100);
+        MultipleCompany1 SBI = new MultipleCompany1 ("SBI",10,10,220);
+        DMART.computeEmpWage( );
+        System.out.println(DMART);
+        SBI.computeEmpWage( );
+        System.out.println(SBI);
+    }
 
 }
+
+
